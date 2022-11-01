15d5ae6e42f531f114a8eb9aa05b0cd144c64ef5
Ankit Jain <aj4451999@gmail.com>
2022-11-01 11:27:14 +0530

    Updated Encapsulation

--- OopsAssignment/Encapsulation.cs
+++ OopsAssignment/Encapsulation.cs
@@ -1,1 +1,31 @@
 ﻿using System;
+
+class Methods
+{
+    public void Type1()
+    {
+        Console.WriteLine("Public Method");
+
+    }
+    private void Type2()
+    {
+        Console.WriteLine("Private Method");
+    }
+
+    protected void Type3()
+    {
+        Console.WriteLine("Protected Method");
+
+    }
+    internal void Type4()
+    {
+        Console.WriteLine("Internal Method");
+
+    }
+
+}
+class Derived : Methods
+{
+
+}
+
